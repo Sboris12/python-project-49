5fd6825519603ddf5b2be6f0ab47a08b82c0e594
Sboris12 <105364457+Sboris12@users.noreply.github.com>
2025-05-23 11:33:19 +0300

    15.5

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,10 @@
+from prompt import string
+
+
 def main():
     print("Welcome to the Brain Games!")
+    name = string("May I have your name? ")
+    print(f"Hello, {name}!")
 
 
 if __name__ == "__main__":
